a61e8cefe37b4c18827c9eaee66aa922267df630
boudefa hamza <boudefahamza@gmail.com>
2022-01-26 03:02:18 +0100

    make it better

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const connectdb=require('./config/connectDB')
+const path= require('path')
 const app=express()
 require('dotenv').config()
 app.use(express.json())
@@ -26,6 +27,14 @@ app.use('/classAPI', require('./routes/classesRoutes'))
 connectdb()
 const port=(process.env.port) || 4001
 
-
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 app.listen(port,()=>console.log(`port listing on ${port}`))

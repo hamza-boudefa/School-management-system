da9a7933fa8a051d12d57dbb5d6172e20cc29d29
boudefa hamza <boudefahamza@gmail.com>
2022-01-30 13:46:29 +0100

    update files

--- app.js
+++ app.js
@@ -27,14 +27,5 @@ app.use('/classAPI', require('./routes/classesRoutes'))
 connectdb()
 const port=(process.env.port) || 4001
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
 
 app.listen(port,()=>console.log(`port listing on ${port}`))
